54e20c1e812b5e123ff1ba05b3c05b541bc61e9c
Aditya Tile <adityatile99@gmail.com>
2024-11-06 21:02:51 +0530

    create express server and test get route

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({
+    message: "get all contacts",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
