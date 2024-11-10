7ba1c961737d71b1b93eb993ffac1eb89a041e98
Aditya Tile <adityatile99@gmail.com>
2024-11-10 21:58:23 +0530

    add register user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 
 // register controller
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("user already registered");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(user);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("user data is not valid");
+  }
   res.json({
     message: "Register the user",
   });
